01181f4b754ad71f1361f29e44fa7aeacab778ff
Tetyana Gogulya <cheburashka077@ukr.net>
2024-06-01 21:01:15 +0300

    1

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,16 @@
-import styles from './App.module.css';
+import { useState, useEffect } from 'react';
 
-const App = () => {};
+import './App.css';
+// import axios from 'axios';
+
+// const ACCESS_KEY = '5jOTyfTQTdUdZH7uunAK7km41pZDP7lSpdm5ob9thZQ';
+
+const App = () => {
+  const [images, setImages] = useState([]);
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [modalImage, setModalImage] = useState(null);
+};
 export default App;
